e7cd87408e4008865e50d8b7dd3184ca8506e4f7
Romanjon Arislanov <romanjon0595@gmail.com>
2022-10-23 18:30:36 +0500

    Second commit cycle

--- JS/script.js
+++ JS/script.js
@@ -10,13 +10,27 @@ const personalMovieDB = {
   privat: false
 };
 
-const a = prompt('One of the latest films you watched?', ''),
-      b = prompt('Rate it?', ''),
-      c = prompt('One of the latest films you watched?', ''),
-      d = prompt('Rate it?', '');
-      
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+  const a = prompt('One of the latest films you watched?', ''),
+        b = prompt('Rate it?', '');
+  
+  if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+    console.log('done');
+  } else {
+    console.log('error');
+    i--;
+  }
+}
 
+if (personalMovieDB.count < 10) {
+  console.log('You have watched a few movies');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  console.log('You are a typical client');
+} else if (personalMovieDB.count >= 30) {
+  console.log('You are a kinoman');
+} else {
+  console.log('Error 404');
+}
 
 console.log(personalMovieDB);
