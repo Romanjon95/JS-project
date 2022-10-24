800d727d5fab9569eed062d12ea7506514bc5fa6
Romanjon Arislanov <romanjon0595@gmail.com>
2022-10-24 21:29:48 +0500

    Third commit functions

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,16 @@
 "use strict";
 
-const numberOfFilms = +prompt('How many films have you watched?', '');
+let numberOfFilms;
+
+function start() {
+  numberOfFilms = +prompt('How many films have you watched?', '');
+
+  while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+    numberOfFilms = +prompt('How many films have you watched?', '');
+  }
+}
+
+start();
 
 const personalMovieDB = {
   count: numberOfFilms,
@@ -10,27 +20,50 @@ const personalMovieDB = {
   privat: false
 };
 
-for (let i = 0; i < 2; i++) {
-  const a = prompt('One of the latest films you watched?', ''),
-        b = prompt('Rate it?', '');
-  
-  if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-    personalMovieDB.movies[a] = b;
-    console.log('done');
-  } else {
-    console.log('error');
-    i--;
+function rememberMyFilms() {
+  for (let i = 0; i < 2; i++) {
+    const a = prompt('One of the latest films you watched?', ''),
+          b = prompt('Rate it?', '');
+    
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+      personalMovieDB.movies[a] = b;
+      console.log('done');
+    } else {
+      console.log('error');
+      i--;
+    }
   }
 }
 
-if (personalMovieDB.count < 10) {
-  console.log('You have watched a few movies');
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-  console.log('You are a typical client');
-} else if (personalMovieDB.count >= 30) {
-  console.log('You are a kinoman');
-} else {
-  console.log('Error 404');
+rememberMyFilms();
+
+function detectPersonalLevel() {
+  if (personalMovieDB.count < 10) {
+    console.log('You have watched a few movies');
+  } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('You are a typical client');
+  } else if (personalMovieDB.count >= 30) {
+    console.log('You are a kinoman');
+  } else {
+    console.log('Error 404');
+  }
 }
 
-console.log(personalMovieDB);
+detectPersonalLevel();
+
+function showMyDB(hidden) {
+  if (!hidden) {
+    console.log(personalMovieDB);
+  }
+}
+
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+  for (let i = 1; i <= 3; i++) {
+    personalMovieDB.genres[i - 1] = prompt(`Your favourite genre number ${i}`);
+  }
+}
+
+writeYourGenres();
+
